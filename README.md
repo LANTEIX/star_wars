833692388f3c67d2aa834c4c0cde8a2cd1458f38
Luiz Antonio <lanteixi@gmail.com>
2019-09-16 13:06:01 +0000

    Versão do final da aula

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # star_wars
-Ap Criado no curso de UI5
+Ap Criado no curso de UI5 aula do dia 02/09/2019
---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,45 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller", // VIRGULA!!!!
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
+	return Controller.extend("ovly.star_wars.controller.View1", {
+
+		identificador: null,
+
+		onInit: function () {
+			this.identificador = this.byId("identificador"); // sap.m.SearchField
+			
+			this.oModel = this.getOwnerComponent().getModel(); // new JSONModel();
+			// this.getView().setModel(this.oModel);
+			this.oModel.attachRequestCompleted(this.onRequestCompleted, this);
+			
+			// @type sap.ui.model.json.JSONModel
+			this.oOptionsModel = this.getOwnerComponent().getModel("options");
+			this.oOptionsModel.setProperty("/id", 1);
+			this.oOptionsModel.setProperty("/ocupado", false);
+			
+			// this.getView().setModel(this.oOptionsModel, "options");
+			 //this.byId("form").setModel(this.oOptionsModel, "options");
+		},
+
+
+		onRequestCompleted: function (oEvent) {
+			this.oOptionsModel.setProperty("/ocupado", false);
+		},
+
+		onPressBuscar: function (oEvent) {
+			// var sId = this.identificador.getValue(); // <------
+			var sId = this.oOptionsModel.getProperty("/id");
+			this.oOptionsModel.setProperty("/ocupado", true);
+
+			this.oModel.loadData("https://swapi.co/api/people/" + sId + "/");
+
+			// nao faz mais o set de ocupado para false
+		},
+
+		onPressDocs: function (oEvent) {
+
+		}
+	});
+});
